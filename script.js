c3541303a01c1db324ad6d2cc734c32acca95874
Adedoyinlorukomi <akindoyin800@gmail.com>
2023-08-26 02:32:28 +0100

    commit new chages

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl)
 })
 
+    
+// $('body').append('<div style="" id="loadingDiv"><div class="loader">Loading...</div></div>');
+// window.on('load', function(){
+//   setTimeout(removeLoader, 200); //wait for page load PLUS two seconds.
+// });
+// function removeLoader(){
+//     $( "#loadingDiv" ).fadeOut(500, function() {
+//       // fadeOut complete. Remove the loading div
+//       $( "#loadingDiv" ).remove(); //makes page more lightweight 
+//   });  
+// }
